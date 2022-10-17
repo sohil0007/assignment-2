bd34719c311112b3ad7fcfdbb392b738ef8e540d
Sohil Mansuri <111635926+sohil0007@users.noreply.github.com>
2022-10-17 22:10:34 +0530

    Add files via upload

---
+++ assignment-2/Q2.py
@@ -0,0 +1,10 @@
+'''
+This is
+my Q2 of
+second assignment
+'''
+a=5
+b=10
+c=15
+d=20
+print(a,b,c,d,sep="\n")
---
+++ assignment-2/Q5.py
@@ -0,0 +1,16 @@
+a=5
+b=10.5
+c="Sohil"
+d=20+5j
+print(a)
+print(type(a))
+print(id(a))
+print(b)
+print(type(b))
+print(id(b))
+print(c)
+print(type(c))
+print(id(c))
+print(d)
+print(type(d))
+print(id(d))
